5db10a5a85f88ef8cbd09bee45a037ab12183053
Mousam Debadatta <mclarenmervin@gmail.com>
2026-09-08 14:07:40 +0530

    Initial Commit

---
+++ Longisync/LongisyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongisyncApp.swift
+//  Longisync
+//
+//  Created by Mousam Debadatta on 08/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LongisyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
